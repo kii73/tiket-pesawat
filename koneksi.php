d78c09b32d22dcca74809d6b30c9aa882d6ee24b
kii73 <kiisaputra271@gmail.com>
2025-09-15 08:52:44 +0700

    mengubah koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,3 +1,4 @@
 <?php
 
-$mysql = new mysqli("localhost", "root", "", "tiket-pesawat");
+// $mysql = new mysqli("localhost", "root", "", "tiket-pesawat");
+$mysql = new mysqli("sql300.infinityfree.com", "if0_39935956", "eVlLaf7Vor9", "if0_39935956_tiketpesawat");
